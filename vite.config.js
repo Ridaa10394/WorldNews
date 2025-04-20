4acdc9b3b9afbc644dcef0cf08d03fbe93bca51f
Ridaa <ridaa.24bcs10394@sst.scaler.com>
2025-04-20 16:08:18 +0530

    Add CDN in Index.html

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/WorldNews/',
+  base:'/WorldNews/',
   plugins: [react(),tailwindcss()],
 })
+
